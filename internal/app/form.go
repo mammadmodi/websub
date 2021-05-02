d7faccb69979c8ae7fc58fce29f561cf9dc18427
mohammadreza.modares <mohammadreza.modares@snapp.cab>
2021-05-03 02:29:15 +0430

    Add Topic input in ws form

--- internal/app/form.go
+++ internal/app/form.go
@@ -28,6 +28,7 @@ var homeTemplate = template.Must(template.New("").Parse(`
 window.addEventListener("load", function(evt) {
     var output = document.getElementById("output");
     var input = document.getElementById("input");
+    var topic = document.getElementById("topic");
     var ws;
     var print = function(message) {
         var d = document.createElement("div");
@@ -47,7 +48,7 @@ window.addEventListener("load", function(evt) {
             ws = null;
         }
         ws.onmessage = function(evt) {
-            print("RESPONSE: " + evt.data);
+            print("Message: " + evt.data);
         }
         ws.onerror = function(evt) {
             print("ERROR: " + evt.data);
@@ -58,8 +59,12 @@ window.addEventListener("load", function(evt) {
         if (!ws) {
             return false;
         }
-        print("SEND: " + input.value);
-        ws.send(input.value);
+        print("SEND: " + input.value + " to topic " + topic.value);
+	    var obj = new Object();
+		obj.body = input.value
+		obj.topic = topic.value
+		var message = JSON.stringify(obj);
+        ws.send(message);
         return false;
     };
     document.getElementById("close").onclick = function(evt) {
@@ -83,7 +88,8 @@ You can change the message and send multiple times.
 <button id="open">Open</button>
 <button id="close">Close</button>
 <p><input id="input" type="text" value="Hello world!">
-<button id="send">Send</button>
+<p><input id="topic" type="text" value="Topic">
+<button id="send">Send Message</button>
 </form>
 </td><td valign="top" width="50%">
 <div id="output"></div>
