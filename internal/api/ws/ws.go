c6d33a9eb21d3aa23ca64a35f09998944412e993
mohammadreza.modares <mohammadreza.modares@snapp.cab>
2021-05-02 20:05:11 +0430

    Use BatchSubscribe method and break Socket method

--- internal/api/ws/ws.go
+++ internal/api/ws/ws.go
@@ -27,44 +27,39 @@ func (m *SockHub) Socket(ctx *gin.Context) {
 	r := ctx.Request
 	w := ctx.Writer
 
+	// validate request and resolve parameters
 	if err := validateRequest(r); err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		_, _ = w.Write([]byte(err.Error()))
 		return
 	}
-
 	// TODO Authenticate and Authorize topics for user
 	un := r.URL.Query().Get("username")
 	topics := strings.Split(r.URL.Query().Get("topics"), ",")
 
-	logrus.WithField("username", un).WithField("topics", topics).Info("request received for user")
-	c, err := m.upgrader.Upgrade(w, r, nil)
+	// upgrade connection
+	wsConn, err := m.upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		logrus.Error("upgrade:", err)
+		logrus.WithField("error", err).WithField("username", un).Error("upgrade error")
 		return
 	}
+	logrus.WithField("username", un).WithField("topics", topics).Info("connection created for user")
 
 	defer func() {
-		err := c.Close()
+		err := wsConn.Close()
 		if err != nil {
 			logrus.WithField("error", err).Fatal("error while closing ws connection")
 		}
 	}()
 
-	logrus.WithField("username", un).Info("connection created for user")
-
-	subs := make([]*hub.Subscription, len(topics))
-
-	for i, t := range topics {
-		s, err := m.Hub.Subscribe(r.Context(), t)
-		// TODO UnSub prev subscription when one error occurs
-		if err != nil {
-			logrus.WithField("err", err).Error("error while subscription")
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		subs[i] = s
+	// create subscription for user topics
+	subs, err := m.Hub.BatchSubscribe(r.Context(), topics)
+	if err != nil {
+		logrus.WithField("username", un).Info("subscriptions failed for user")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
+	logrus.WithField("username", un).Info("subscriptions created for user")
 
 	defer func() {
 		for _, s := range subs {
@@ -72,8 +67,27 @@ func (m *SockHub) Socket(ctx *gin.Context) {
 		}
 	}()
 
-	logrus.WithField("username", un).Info("subscriptions created for user")
+	go writer(un, wsConn, subs)
+	reader(wsConn)
+}
 
+func reader(conn *websocket.Conn) {
+	// read user sent messages
+	for {
+		mt, message, err := conn.ReadMessage()
+		if err != nil {
+			logrus.Println("read:", err)
+			break
+		}
+
+		logrus.
+			WithField("message_type", mt).
+			WithField("received_message", string(message)).
+			Info("message received from user")
+	}
+}
+
+func writer(un string, conn *websocket.Conn, subs []*hub.Subscription) {
 	for _, s := range subs {
 		// pass redis messages to user
 		go func(s *hub.Subscription) {
@@ -83,35 +97,18 @@ func (m *SockHub) Socket(ctx *gin.Context) {
 					WithField("payload", msg.Payload).
 					Info("message received from redis")
 
-				err = c.WriteMessage(1, []byte(msg.Payload))
+				err := conn.WriteMessage(1, []byte(msg.Payload))
 				if err != nil {
 					logrus.WithField("error", err).Error("error while sending message to user")
 					break
 				}
 			}
 		}(s)
-		logrus.WithField("username", un).WithField("username", s.Topic).Info("subscription created")
+		logrus.
+			WithField("username", un).
+			WithField("topics", s.Topic).
+			Info("subscription created")
 	}
-
-	go func() {
-		// read user sent messages
-		for {
-			mt, message, err := c.ReadMessage()
-			if err != nil {
-				logrus.Println("read:", err)
-				break
-			}
-
-			logrus.
-				WithField("message_type", mt).
-				WithField("received_message", string(message)).
-				Info("message received from user")
-		}
-	}()
-
-	// TODO Handle close in a proper way
-	<-r.Context().Done()
-	logrus.WithField("username", un).Info("connection closed for user")
 }
 
 func validateRequest(req *http.Request) error {
