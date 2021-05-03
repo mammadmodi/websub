729592631f1b3e6245d6466b3aea4eaa7e70a0ac
mohammadreza.modares <mohammadreza.modares@snapp.cab>
2021-05-04 01:51:24 +0430

    Remove extra configs

--- internal/app/configs.go
+++ internal/app/configs.go
@@ -6,23 +6,17 @@ import (
 	"github.com/mammadmodi/webis/internal/api/websocket"
 	"github.com/mammadmodi/webis/pkg/logger"
 	"github.com/mammadmodi/webis/pkg/redis"
-	"io/ioutil"
 	"time"
 )
 
 // Configs is struct that contains all configuration of all parts of application
 type Configs struct {
-	SockHubConfig       websocket.Configuration
-	RedisConfigs        redis.Configs
-	LoggingConfigs      logger.Configuration
-	AuthMethod          string        `default:"jwt" split_words:"true"`
-	JwtRsaPublicKeyPath string        `default:"/var/lib/webis/keys" split_words:"true"`
-	Mode                string        `default:"debug"`
-	Addr                string        `default:"127.0.0.1"`
-	Port                int           `default:"8379"`
-	GracefulTimeout     time.Duration `default:"15s" split_words:"true"`
-
-	RSAPublicKey []byte
+	SockHubConfig   websocket.Configuration
+	RedisConfigs    redis.Configs
+	LoggingConfigs  logger.Configuration
+	Addr            string        `default:"127.0.0.1"`
+	Port            int           `default:"8379"`
+	GracefulTimeout time.Duration `default:"15s" split_words:"true"`
 }
 
 // NewConfiguration returns a configuration that is loaded with environment variables
@@ -56,21 +50,6 @@ func NewConfiguration() (*Configs, error) {
 		return nil, fmt.Errorf("error while processing redis client configs from env variables, error: %v", err)
 	}
 	config.RedisConfigs = redisConfigs
-	//
-	//// Load RSA public keys to config object
-	//rsaPubKey, err := getRSAPublicKeys(config.JwtRsaPublicKeyPath)
-	//if err != nil {
-	//	return nil, fmt.Errorf("error while loading rsa public keys to config, error: %v", err)
-	//}
-	//config.RSAPublicKey = rsaPubKey
 
 	return config, nil
-}
-
-func getRSAPublicKeys(jwtRSAPath string) ([]byte, error) {
-	key, err := ioutil.ReadFile(jwtRSAPath)
-	if err != nil {
-		return nil, fmt.Errorf("cannot read public key from %s", jwtRSAPath)
-	}
-	return key, nil
 }
